72428859c1123560685d711ee1aafe680e065065
Camelia <cameliabraghes@gmail.com>
2023-06-07 11:25:55 +0300

    Add project files

---
+++ FreaksList/FreaksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreaksListApp.swift
+//  FreaksList
+//
+//  Created by Camelia Braghes on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FreaksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
