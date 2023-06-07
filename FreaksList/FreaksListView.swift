4ab433496131150ab02865cd4b61eb9fa6f97792
Camelia <cameliabraghes@gmail.com>
2023-06-07 13:11:50 +0300

    Create FreaksListView

---
+++ FreaksList/FreaksListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FreaksListView: View {
+    var body: some View {
+        Text("Hello, Freaks!")
+    }
+}
+
+struct FreaksListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FreaksListView()
+    }
+}
